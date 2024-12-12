5ddc8b3d22760955e25e6696f2b3703998b3d581
ArminKardan <armin.fire@gmail.com>
2024-12-12 06:05:47 +0330

    Update

--- backend/ROOT/Schedule/S30.ts
+++ backend/ROOT/Schedule/S30.ts
@@ -2,11 +2,5 @@ export default async () => {
     setInterval(async () => {
         //    let u = await API.test({id:4})
 
-        let items = await db.collection("config").find({}).project({ _id: 0 }).toArray()
-        for (let item of items) {
-            if (Object.keys(item).length == 1) {
-                global[Object.keys(item)[0]] = item[Object.keys(item)[0]]
-            }
-        }
     }, 30000)
 }
