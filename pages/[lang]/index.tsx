8a56168b0a6c0a4cdcb4a85ea1b6bdb80833c044
ArminKardan <armin.fire@gmail.com>
2024-12-13 22:02:28 +0330

    Update

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -10,8 +10,41 @@ export default p => Component(p, Page);
 
 const Page: PageEl = (props, refresh, getProps, dies, z) => {
 
-  return <Window title="my page" style={{ }}>
-    <br-x />
+  getProps(async () => {
+    props.counter = 0
+
+    props.asc = true
+    let c = setInterval(() => {
+      if(props.asc)
+      {
+        props.counter++;
+      }
+      else
+      {
+        props.counter--
+      }
+      if (props.counter >= 10) {
+        props.asc = false
+      }
+      if(props.counter < 0)
+      {
+        clearInterval(c)
+      }
+
+      refresh()
+    }, 1000);
+  })
+
+
+  return <Window title="my page" style={{ height: "900px" }}>
+
+    <b-200>{props.counter}</b-200>
+
+
+    {/* <pre>{JSON.stringify(props,null,2)}</pre> */}
+
+    {/* <pre>{JSON.stringify(z.user,null, 2)}</pre> */}
+    {/* <br-x />
     <b-200 onClick={async () => { await alerter("hi!") }}>Alerter</b-200>
     <br-xxx />
     <b-200 onClick={async () => { Copy(z.user.uid); success("کپی شد") }}>Copy UID</b-200>
@@ -19,9 +52,7 @@ const Page: PageEl = (props, refresh, getProps, dies, z) => {
     <b-200 onClick={async () => { Router.push(z.root+"/admin")}}>Admin Page</b-200>
     <br-xxx />
     <b-200 onClick={async () => { Router.push(z.root+"/calculator") }}>Calculator</b-200>
-    <br-xxx />
-    <b-200 onClick={async () => { let json = await API["ping"](null); alerter(json)}}>API</b-200>
-    <pre style={{ fontSize: 12 }}>{JSON.stringify(z.user, null, 2)}</pre>
+    <pre style={{ fontSize: 12 }}>{JSON.stringify(z.user, null, 2)}</pre> */}
   </Window>
 }
 
