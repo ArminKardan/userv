d241632e5d61f3f3ec77f6c46fcf48554b0cab38
ArminKardan <armin.fire@gmail.com>
2025-01-20 10:20:03 +0330

    Update

--- pages/[lang]/workertest.tsx
+++ pages/[lang]/workertest.tsx
@@ -91,6 +91,12 @@ const Page: PageEl = (props, refresh, getProps, onConnected, dies, z) => {
     }}>selector something</b-200>
 
     <b-200 onClick={async () => {
+      await alerter("MD5 HASH: " + MD5("hiiii"))
+      await alerter("SHA256 HASH: " + SHA256("hiiii"))
+    }}>MD5 hash</b-200>
+
+
+    <b-200 onClick={async () => {
       Router.push("/")
     }}>goto index</b-200>
 
