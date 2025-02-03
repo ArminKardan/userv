9d5c759c5ab3a98e100c0d9600fe5571d766567e
ArminKardan <armin.fire@gmail.com>
2025-02-03 12:51:04 +0330

    Update

--- backend/API/print.ts
+++ backend/API/print.ts
@@ -1,2 +1,7 @@
 type T = Parameters<typeof F>[0]; type R = ReturnType<typeof F>
 declare global { interface API { "print": (T: T) => R } var API: API }
+
+
+export default async function F(T: null, C: APISession,) {
+  console.log("Hello world!")
+}
