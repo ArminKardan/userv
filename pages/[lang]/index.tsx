e4c037264bf8386111838539968a04f0885a0cc4
ArminKardan <armin.fire@gmail.com>
2025-02-03 16:24:06 +0330

    Update

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -13,6 +13,13 @@ const Page: PageEl = (props, refresh, getProps, onConnected, dies, z) => {
 
   return <div>
 
+    <Window title="Dialog Examples" style={{ paddingBottom: 10 }}>
+      <w-cc style={{ gap: 5, padding: 5 }}>
+        <b-200 onClick={async () => { alerter("Hello world") }}>Alerter</b-200>
+        <b-200 onClick={async () => { alerter("Hello world") }}>Alerter</b-200>
+      </w-cc>
+    </Window>
+
     <Window title="مثال مقدماتی" style={{ paddingBottom: 10 }}>
 
       {/* <pre>{JSON.stringify(z.user, null, 2)}</pre> */}
@@ -60,14 +67,7 @@ const Page: PageEl = (props, refresh, getProps, onConnected, dies, z) => {
       </w-cc>
     </Window>
 
-    <Window title="Worker Examples" style={{ paddingBottom: 10 }}>
-      <w-cc style={{ gap: 5, padding: 5 }}>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/insert") }}></b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/delete") }}></b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/query") }}></b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/update") }}></b-200>
-      </w-cc>
-    </Window>
+
 
   </div>
 }
