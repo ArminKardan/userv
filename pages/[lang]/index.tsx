b870b5bde2559a76a9f855bfa05c6d073bfeffdd
ArminKardan <armin.fire@gmail.com>
2025-02-03 16:22:06 +0330

    Update

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -62,10 +62,10 @@ const Page: PageEl = (props, refresh, getProps, onConnected, dies, z) => {
 
     <Window title="Worker Examples" style={{ paddingBottom: 10 }}>
       <w-cc style={{ gap: 5, padding: 5 }}>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/insert") }}>اضافه کردن داکیومنت</b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/delete") }}>حذف داکیومنت</b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/query") }}>اجرای کوئری</b-200>
-        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/update") }}>تغییر داکیومنت</b-200>
+        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/insert") }}></b-200>
+        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/delete") }}></b-200>
+        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/query") }}></b-200>
+        <b-200 onClick={async () => { Router.push(z.root + "/examples/db/update") }}></b-200>
       </w-cc>
     </Window>
 
