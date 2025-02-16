633c662362e5cdc125f7c34358a9dec2739b729a
ArminKardan <armin.fire@gmail.com>
2025-02-16 18:14:17 +0330

    Update

--- frontend/components/qecomps/FaDigits.tsx
+++ frontend/components/qecomps/FaDigits.tsx
@@ -17,3 +17,21 @@ export default (input) => {
   .replace(/9/g,"۹")
 }
 
+
+export const EnDigits = (input) => {
+
+  if(typeof input !="string")
+  {
+    return "Not a number"
+  }
+  return input.replace(/0/g,"0")
+  .replace(/۱/g,"1")
+  .replace(/۲/g,"2")
+  .replace(/۳/g,"3")
+  .replace(/۴/g,"4")
+  .replace(/۵/g,"5")
+  .replace(/۶/g,"6")
+  .replace(/۷/g,"7")
+  .replace(/۸/g,"8")
+  .replace(/۹/g,"9")
+}
